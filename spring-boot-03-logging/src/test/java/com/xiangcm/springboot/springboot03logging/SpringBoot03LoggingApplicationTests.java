00a56c647c05dac230aee8338669ac3f6bcf22c9
xiangcm1 <xiangcm1163@163.com>
2021-02-07 22:43:18 +0800

    SpringBoot03 logging

---
+++ spring-boot-03-logging/src/test/java/com/xiangcm/springboot/springboot03logging/SpringBoot03LoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiangcm.springboot.springboot03logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot03LoggingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
